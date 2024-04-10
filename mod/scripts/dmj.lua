5b7f884664f28a92bdb877d56092c72f11c047b8
好黑好黑的大黑 <1290095285@qq.com>
2024-04-11 01:44:57 +0800

    崩溃修复/增加粉丝牌等级适配
    1. 修复变量v未定义导致的崩溃。
    2. 对佩戴了粉丝牌的观众新增头像图标适配。
    3. 增加了大航海用户的昵称颜色匹配（无：白/舰长：蓝/提督：金/总督：红）（未测试，快去抓个有舰长的主播测试）。

--- mod/scripts/dmj.lua
+++ mod/scripts/dmj.lua
@@ -45,28 +45,50 @@ function DMJ_Start()
     TheWorld:DoTaskInTime(0.1, DMJ_Fetch)
 end
 
+local nameColour = {}
+nameColour[0] = UICOLOURS.WHITE
+nameColour[1] = UICOLOURS.BLUE
+nameColour[2] = UICOLOURS.GOLD
+nameColour[3] = UICOLOURS.RED
+
 function DMJ_Fetch()
     local baseurl = Settings.baseurl or "http://127.0.0.1:9876"
     TheSim:QueryServer(baseurl .. "/room/" .. Settings.roomcode .. "/msgs",
-        function(result, isSuccessful, code)
-            if isSuccessful and string.len(result) > 1 and code == 200 then
-                State = "Running"
-                local status, data = pcall(function() return json.decode(result) end)
-                if not status or not data then
-                    return
-                end
-                for k, c in ipairs(data) do
-                    if c.cmd == "LIVE_OPEN_PLATFORM_DM" then
-                        v = c.data
-                        if v.uname and v.msg then
-                            ChatHistory:AddToHistory(ChatTypes.Message, nil, nil, v.uname, v.msg, WHITE,
-                                "profileflair_skincollector", nil, true)
+            function(result, isSuccessful, code)
+                if isSuccessful and string.len(result) > 1 and code == 200 then
+                    State = "Running"
+                    local status, data = pcall(function() return json.decode(result) end)
+                    if not status or not data then
+                        return
+                    end
+                    for k, c in ipairs(data) do
+                        if c.cmd == "LIVE_OPEN_PLATFORM_DM" then
+                            local v = c.data
+                            if v.uname and v.msg then
+                                -- 是否增加几个mod选项，供选择头像等级组合？
+                                if v.fans_medal_level and v.fans_medal_level ~= 0 and v.fans_medal_wearing_status then
+                                    if v.fans_medal_level < 10 then
+                                        ChatHistory:AddToHistory(ChatTypes.Message, nil, nil, v.uname, v.msg, nameColour[v.guard_level or 0],
+                                                "profileflair_egg", nil, true) -- 鸟蛋
+                                    elseif v.fans_medal_level < 20 then
+                                        ChatHistory:AddToHistory(ChatTypes.Message, nil, nil, v.uname, v.msg, nameColour[v.guard_level or 0],
+                                                "profileflair_crowkid", nil, true) -- 鸦年华小乌鸦
+                                    else
+                                        ChatHistory:AddToHistory(ChatTypes.Message, nil, nil, v.uname, v.msg, nameColour[v.guard_level or 0],
+                                                "profileflair_corvus", nil, true) -- 鸦年华良羽鸦
+                                    end
+                                else
+                                    ChatHistory:AddToHistory(ChatTypes.Message, nil, nil, v.uname, v.msg, nameColour[v.guard_level or 0],
+                                            "profileflair_skincollector", nil, true)
+                                end
+                            end
+                        elseif c.cmd == "LIVE_OPEN_PLATFORM_SEND_GIFT" then
+                            -- 预留给
                         end
                     end
+                    TheWorld:DoTaskInTime(0.1, DMJ_Fetch)
+                else
+                    State = "Error"
                 end
-                TheWorld:DoTaskInTime(0.1, DMJ_Fetch)
-            else
-                State = "Error"
-            end
-        end)
+            end)
 end
