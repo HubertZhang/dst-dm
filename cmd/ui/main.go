63e09580e7087da18b30975e70446088e14edf5c
Hubert Zhang <hubert.zyk@gmail.com>
2024-05-15 00:41:05 +0800

    SingleRoomUI

---
+++ cmd/ui/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/browser"
+	"github.com/vtb-link/bianka/basic"
+	webview "github.com/webview/webview_go"
+	"golang.org/x/exp/slog"
+)
+
+var logger *slog.Logger
+
+var (
+	flagPort = flag.Int("port", 9876, "监听端口")
+)
+
+// 从直播开放平台获取的 accessKey
+var AccessKey string
+
+// 从直播开放平台获取的 secret
+var AccessToken string
+
+var AppIdStr string
+
+var AppId int64
+
+func init() {
+	var err error
+	AppId, err = strconv.ParseInt(AppIdStr, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+}
+
+var config struct {
+	RoomCode string
+	SaveCode bool
+}
+
+//go:embed index.html
+var index string
+
+func main() {
+	logger = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	basic.DefaultLoggerGenerator = func() *slog.Logger {
+		return logger.With("module", "bianka")
+	}
+	d, err := os.ReadFile("config.json")
+	if err == nil {
+		json.Unmarshal(d, &config)
+	}
+
+	if config.SaveCode {
+		index = strings.Replace(index, `var roomCode = "";`, `var roomCode = "`+config.RoomCode+`";`, 1)
+		index = strings.Replace(index, `var saveCode = false;`, `var saveCode = true;`, 1)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	w := webview.New(false)
+	defer w.Destroy()
+	w.SetTitle("饥荒弹幕机")
+	w.SetSize(420, 300, webview.HintFixed)
+
+	err = w.Bind("callback", func(roomCode string, save bool) error {
+		if save {
+			config.RoomCode = roomCode
+			config.SaveCode = true
+			d, err := json.Marshal(config)
+			if err != nil {
+				return err
+			}
+			err = os.WriteFile("config.json", d, 0644)
+			if err != nil {
+				return err
+			}
+		} else {
+			config.RoomCode = roomCode
+		}
+		fmt.Println(config.RoomCode)
+		w.Eval(`setState("processing")`)
+		go func() {
+			err := startServer(ctx, config.RoomCode, func() {
+				w.Eval(`setState("error", "转发已终止")`)
+			})
+			if err != nil {
+				w.Eval(`setState("error", "转发已终止")`)
+				return
+			}
+			w.Eval(`setState("running")`)
+		}()
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	w.Bind("stop", func(req ...any) error {
+		cancel()
+		return nil
+	})
+	w.Bind("openRoom", func(req ...any) error {
+		return browser.OpenURL("https://link.bilibili.com/p/center/index#/my-room/start-live")
+	})
+	w.SetHtml(index)
+	w.Run()
+}
