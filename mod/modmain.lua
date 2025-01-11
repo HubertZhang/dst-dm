48c46eacf4f641c9d0305c737a8a4c4557fec714
Hubert Zhang <hubert.zyk@gmail.com>
2025-01-11 16:44:28 +0800

    Add configuration for enter room info

--- mod/modmain.lua
+++ mod/modmain.lua
@@ -2,6 +2,18 @@ GLOBAL.setmetatable(env, {
     __index = function(t, k) return GLOBAL.rawget(GLOBAL, k) end
 })
 
+GLOBAL.DMJ = {}
+GLOBAL.DMJ.Configurations = {}
+
+local options = {
+    "show_enter"
+}
+
+for index, value in ipairs(options) do
+    local m = GetModConfigData(value)
+    GLOBAL.DMJ.Configurations[value] = m
+end
+
 require("dmj")
 
 AddSimPostInit(function()
